65bb542264667baa1bc0a54f7fef8d82bc6421af
ruscorreia <rusacorreia@hotmail.com>
2024-06-24 10:45:29 +0100

    Update Chat.tsx

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -227,7 +227,7 @@ const Chat = () => {
                                 <>
                                     <UserChatMessage message={lastQuestionRef.current} />
                                     <div className={styles.chatMessageGptMinWidth}>
-                                        <AnswerError error={error.toString()} onRetry={() => makeApiRequest(lastQuestionRef.current)} />
+                                        <AnswerError error={error.toString().replace("Internal Server Error","P.f. volte a tentar")} onRetry={() => makeApiRequest(lastQuestionRef.current)} />
                                     </div>
                                 </>
                             ) : null}
